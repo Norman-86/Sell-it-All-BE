a1b65a79a0788587c3f48e37c5a2aaa611ce51db
Collins Park <normanpark85@outlook.com>
2021-10-22 10:43:47 +0300

    added average ratings for products

--- models/products.js
+++ models/products.js
@@ -31,8 +31,23 @@ const productSchema = new Schema({
       type: Schema.Types.ObjectId,
       ref: 'Review'
     }
-  ]
+  ],
+  avgRating: {type: Number, default: 0}
 });
 
+productSchema.methods.calculateAvgRating = function () {
+  let ratingsTotal = 0;
+  if (this.reviews.length) {
+    this.reviews.forEach(review => {
+      ratingsTotal += review.rating;
+    });
+    this.avgRating = Math.round((ratingsTotal / this.reviews.length) * 10) / 10;
+  } else {
+    this.avgRating = ratingsTotal;
+  }
+  const floorRating = Math.floor(this.avgRating);
+  this.save();
+  return floorRating;
+}
 const Product = mongoose.model('Product', productSchema);
 module.exports = Product;
